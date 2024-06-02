db3c17cc972f4ad9122a8063ff2accc032d73b2b
Gamerranxx ✔ <armin.muric56@gmail.com>
2024-06-02 18:10:05 +0200

    Structure updated

--- Frontend-WebApp/Frontend-WebApp/Data/ApplicationUser.cs
+++ Frontend-WebApp/Frontend-WebApp/Data/ApplicationUser.cs
@@ -1,10 +1,25 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
-namespace Frontend_WebApp.Data
-{
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
-    {
-    }
+namespace Frontend_WebApp.Data;
 
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string? Biography { get; set; }
+    public string? ProfileImage { get; set; } = "avatar.jpg";
+    public int? AddressId { get; set; }
+    public AddressEntity? Address { get; set; }
+}
+
+public class AddressEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public string AdressLine_1 { get; set; } = null!;
+    public string? AdressLine_2 { get; set; }
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
 }

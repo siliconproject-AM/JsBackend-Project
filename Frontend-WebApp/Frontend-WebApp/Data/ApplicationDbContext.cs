799d73590dbacc8d162941b93ad3443fe037cbc6
Gamerranxx ✔ <armin.muric56@gmail.com>
2024-06-03 02:57:33 +0200

    Structure updated

--- Frontend-WebApp/Frontend-WebApp/Data/ApplicationDbContext.cs
+++ Frontend-WebApp/Frontend-WebApp/Data/ApplicationDbContext.cs
@@ -5,5 +5,6 @@ namespace Frontend_WebApp.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<AddressEntity> Addresses { get; set; }
     }
 }
